8263c7d7f7abed7d4fb14e978d80ab1b3f39be85
Angus Fletcher <angusiguess@gmail.com>
2013-09-21 19:12:57 -0300

    FIRST!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plungr::Application.config.secret_key_base = 'b9ab4c30f8434565032621e47b0f83c61f2a9e9ebcce4bae150e36edab71d9f2471fa2f27f18deb1941e55a038c3c5fdf40076ed88ce6b908756eb6679a945a0'
