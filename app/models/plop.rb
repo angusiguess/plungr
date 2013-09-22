cb4d69f2f89fcb42351f9a5ebe7fc99a6b911a97
Ian Bishop <bishop14@gmail.com>
2013-09-22 13:34:50 -0300

    stalls

--- app/models/plop.rb
+++ app/models/plop.rb
@@ -1,4 +1,4 @@
 class Plop < ActiveRecord::Base
     belongs_to :customer
-  scope :trending, lambda { |limit| order("created_at DESC").limit(limit) }
+    scope :trending, lambda { |limit| order("created_at DESC").limit(limit) }
 end
