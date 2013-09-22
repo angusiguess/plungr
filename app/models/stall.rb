fc9d69e447bc6a80d5ffb9da47e1f14a24047661
Ian Bishop <bishop14@gmail.com>
2013-09-22 09:56:21 -0300

    fancied it up

--- app/models/stall.rb
+++ app/models/stall.rb
@@ -7,7 +7,7 @@ class Stall < ActiveRecord::Base
     super(*stall_params)
     self.uuid = generate_uuid
     self.write_hash = generate_write_hash(self.uuid)
-    qr_code_img = RQRCode::QRCode.new("http://www.plungr.com/qr/#{uuid}?write_hash=#{write_hash}", :size => 16, :level => :h).to_img
+    qr_code_img = RQRCode::QRCode.new("http://www.plungr.com/qr/#{uuid}?write_hash=#{write_hash}", :size => 16, :level => :h).to_img.resize(500, 500)
     self.update_attribute :qr_code, qr_code_img.to_string
   end
 
