da6a08fe700a73da6b55ac3b59a251e15f5c787a
dbrenden <brenden.doyle@gmail.com>
2013-09-21 21:27:10 -0300

    Stall now generates UUID and write_hash on initialization

--- app/models/stall.rb
+++ app/models/stall.rb
@@ -1,2 +1,19 @@
+require 'digest/sha1'
+
 class Stall < ActiveRecord::Base
+
+	def initialize(*stall_params)
+		super(*stall_params)
+		generateUUID
+		generateWriteHash
+	end
+
+	def generateUUID
+		self.uuid = SecureRandom.uuid
+	end
+
+	def generateWriteHash
+		self.write_hash = Digest::SHA1.hexdigest self.uuid
+	end
+
 end
