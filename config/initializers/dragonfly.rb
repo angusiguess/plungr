9924e5ad13b9dccc64537377c8d37fd62dab966a
Angus Fletcher <angusiguess@gmail.com>
2013-09-22 13:17:11 -0300

    Fight with dragonfly for heroku

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,5 +3,13 @@ require 'dragonfly'
 app = Dragonfly[:images]
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
-app.configure_with(:heroku, 'plungr') if Rails.env.production?
+if Rails.env.production?
+  app.configure do |c|
+    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+      :bucket_name => 'plungr',
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    )
+  end
+end
 app.define_macro(ActiveRecord::Base, :image_accessor)
