25a3b0d4d2cf912d6b752f16a165874d40ffafc7
Angus Fletcher <angusiguess@gmail.com>
2013-09-22 14:41:38 -0300

    Add trending

--- app/models/plop.rb
+++ app/models/plop.rb
@@ -1,4 +1,5 @@
 class Plop < ActiveRecord::Base
     belongs_to :customer
-    scope :trending, lambda { |limit| order("created_at DESC").limit(limit) }
+    scope :trending, lambda { |limit| order("heart_count DESC").limit(limit) }
+    scope :latest, lambda { |limit| order("created_at DESC").limit(limit) }
 end
