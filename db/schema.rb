0cdab093bedcdc1ca0fc4274d21ef5e62b91825a
Angus Fletcher <angusiguess@gmail.com>
2013-09-22 10:37:37 -0300

    Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922121046) do
+ActiveRecord::Schema.define(version: 20130922123723) do
 
   create_table "plops", force: true do |t|
     t.string   "message"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "stall_id"
+    t.string   "author_name"
   end
 
   create_table "stalls", force: true do |t|
